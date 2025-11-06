10699ae24324c5fabf5deabf6dc9ede653001ce8
AnneLundM <anne@mediacollege.dk>
2025-11-06 07:07:49 +0100

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import PageHeader from "./components/pageHeader/PageHeader";
 
+// Forælder/parent-komponenten styrer hvornår barnet/child vises
 function App() {
   return (
     <>
