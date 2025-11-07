2e7ecf57adf82799a4bf690ae278efa5a92a5aff
AnneLundM <anne@mediacollege.dk>
2025-11-07 12:48:20 +0100

    comments

--- src/components/pageHeader/PageHeader.jsx
+++ src/components/pageHeader/PageHeader.jsx
@@ -4,8 +4,10 @@ import styles from "./pageHeader.module.css";
 import logo from "/logo.png";
 
 const PageHeader = () => {
+  /* State variabel der deklares med Reacts indbyggede hook: useState. Dens initialvalue/standardværdi/defaultvalue er false */
   const [open, setOpen] = useState(false);
 
+  // Funktion der setter state til dens modsatte, nuværende værdi.
   const toggle = () => {
     setOpen(!open);
   };
@@ -16,6 +18,7 @@ const PageHeader = () => {
       <h1>
         Gittes <span>Glamping</span>
       </h1>
+      {/* Vi sender 3 props videre med komponenten: Én til teksten, én til funktionen og én til dens style  */}
       <Button buttonText='Book nu' onClick={toggle} variant='transparent' />
     </header>
   );
