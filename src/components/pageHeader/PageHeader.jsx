1df8f4fae84e122ea37210597c1dd3a178fc9810
AnneLundM <anne@mediacollege.dk>
2025-11-12 17:21:57 +0100

    12/11

--- src/components/pageHeader/PageHeader.jsx
+++ src/components/pageHeader/PageHeader.jsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
 import Button from "../button/Button";
 import styles from "./pageHeader.module.css";
+import { useNavigate } from "react-router";
 
-const PageHeader = ({ logo, titleOne, titleTwo, button }) => {
-  /* State variabel der deklares med Reacts indbyggede hook: useState. Dens initialvalue/standardværdi/defaultvalue er false */
-  const [open, setOpen] = useState(false);
-
-  // Funktion der setter state til dens modsatte, nuværende værdi.
-  const toggle = () => {
-    setOpen(!open);
-  };
+const PageHeader = ({ logo, titleOne, titleTwo, button, bgImg }) => {
+  const navigate = useNavigate();
 
   return (
-    <header className={styles.header}>
+    <header
+      className={styles.header}
+      style={{ backgroundImage: `url(${bgImg})` }}>
       {/* Hvis logo er true, skal <img/> vises/renderes. Hvis den er false (eller undefined eller null) skal den ikke. */}
       {logo && <img src={logo} alt='logo' />}
 
@@ -26,7 +22,11 @@ const PageHeader = ({ logo, titleOne, titleTwo, button }) => {
       {/* Vi sender 3 props videre med komponenten: Én til teksten, én til funktionen og én til dens style  */}
 
       {button && (
-        <Button buttonText='Book nu' onClick={toggle} variant='transparent' />
+        <Button
+          buttonText='Book nu'
+          onClick={() => navigate("/stays")}
+          variant='transparent'
+        />
       )}
     </header>
   );
